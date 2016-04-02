bc72be47da97b7bbedee7943aebc6959655d2f31
revinsys <revinsys@Mac-Pro-revinsys.local>
2016-04-02 12:11:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+h1 Rev-router
+=====================
+Простой маршрутизатор для приложений построенные на nodejs, без использования каких либо фреймворков.
+На данный момент функционал реализован максимально просто. 
+
+h2 Использование
+-----------------------------------
+Подключите модуль в Вашем проекте
+```js
+	var router = require('rev-router');
+```
+
+Добавляете все необходимые пути отслеживания, например:
+```js
+	router.get('/', function(req, res){
+		/* Исполняемый блок */
+	});
+
+	router.post('/', function(req, res){
+		/* Исполняемый блок */
+	});
+
+	router.put('/', function(req, res){
+		/* Исполняемый блок */
+	});
+
+	router.del('/', function(req, res){
+		/* Исполняемый блок */
+	});
+```
+Так же возможно добавление путей с "регулярными" выражениями, расширяющий объект req, пример:
+```js
+	router.get('/users/:id', function(req, res){
+		console.log(req.params.id); // При переходе по пути /users/125 выведет в консоль 125
+	});
+```
+После чего нужно запустить выполнение маршрутизатора
+```js
+	http.createServer(router.start).listen(4444);
+```
+
+h2 Пример использования
+-----------------------------------
+```js
+	var http = require('http'),
+		router = require('rev-router');
+
+		router.get('/', function(req, res){
+				res.end('Home Page');
+			});
+
+		router.get('/users/:id', function(req, res){
+				res.end('Welcom, user '+req.params.id);
+			});
+
+		http.createServer(function(req, res){
+			    if (!router.start(req, res)){
+	    		    res.end('Not Found!');
+	    		}
+			}).listen(4444);
+```
---
+++ router.js
@@ -0,0 +1,71 @@
+var Router = {
+    router: [],
+    get: get,
+    post: post,
+    put: put,
+    del: del,
+    start: start
+}
+
+
+/*
+ * Добавляем пути и вызываемые функции в массив маршрутизации
+ */
+function get(uri, funct) {
+    this.router['GET'+uri] = funct;
+}
+
+function post(uri, funct) {
+    this.router['POST'+uri] = funct;
+}
+
+function put(uri, funct) {
+    this.router['PUT'+uri] = funct;
+}
+
+function del(uri, funct) {
+    this.router['DELETE'+uri] = funct;
+}
+
+
+/*
+ * Выполнение роутинга по ключу с учетом регулярных выражений, расширение объекта req свойством params
+ */
+function start(req, res) {
+    var query = req.method+req.url;
+
+    if (this.router[query] === undefined) {
+
+        for (var key in this.router) {
+            if (/:[A-Za-z0-9]*/.test(key)) {
+                var tmpvar = key.match(/:[A-Za-z0-9]*/g);
+                var result = key.replace( /:[A-Za-z0-9]*/g, '[A-Za-z0-9]*');
+                var tmpval = key.replace( /:[A-Za-z0-9]*/g, '([A-Za-z0-9]*)');
+
+                var expr = new RegExp(result, 'g');
+                var valexpr = new RegExp(tmpval);
+
+                var resul = expr.exec(query);
+                var valres = valexpr.exec(query);
+
+                if (resul == query) {
+
+                    req.params = {};
+                    for (var i=0; i<tmpvar.length; i++) {
+                        req.params[tmpvar[i].substring(1, tmpvar[i].length)] = valres[i+1];
+                    }
+                    this.router[key](req, res);
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    } else {
+        this.router[query](req, res);
+        return true;
+    }
+
+}
+
+module.exports = Router;
